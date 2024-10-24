a663a724b08cf151980d14f498da90184ec8cb13
Évelim Dornelles <dorrrnelles@gmail.com>
2024-10-24 15:03:55 -0300

    upload

--- src/engine.js
+++ src/engine.js
@@ -1,24 +1,23 @@
 new Swiper('.card-wrapper', {
     loop: true,
-    spaceBetween: 30,
+    spaceBetween: 3,
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
 
-
     breakpoints: {
         0: {
-            sliderPerView: 1
+            slidesPerView: 1  // Corrigido para 'slidesPerView'
         },
 
         768: {
-            sliderPerView: 2
+            slidesPerView: 2  // Corrigido para 'slidesPerView'
         },
 
         1024: {
-            sliderPerView: 3
+            slidesPerView: 4  // Corrigido para 'slidesPerView'
         },
     }
 });
